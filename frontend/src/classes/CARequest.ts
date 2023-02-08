4d23c93d52e7a92082de994d3b8266cf5e5bf8b8
Anthony Bryson <antbryson17@gmail.com>
2023-02-08 15:20:45 -0800

    Convert to ts

--- frontend/src/classes/CARequest.ts
+++ frontend/src/classes/CARequest.ts
@@ -3,10 +3,12 @@
 /*  FUNCTIONS
     setActiveQuarter(quarter)       changes which quarter will be used for queries
                                     (valid params: "Fall 2021", "Summer 2022", etc.)
-    async getSearchResults(query)   returns a list of all the section that match the query
-    async getCourseList()           returns a list of all courses being offered (use this 
-                                    to autofill the user's input 
-    async getQuarters()             returns a list of all quarters and their id's
+    async getSearchResults(query)   returns a list of all the section that match the query,
+                                    or an empty array if it failed
+    async getCourseList()           returns a list of all courses being offered, or an 
+                                    empty array if it failed (use this to autofill the user's input)
+    async getQuarters()             returns a list of all quarters and their id's, or an 
+                                    empty array if it failed
 */
 
 /*  DEMO
@@ -21,9 +23,9 @@
 */
 
 export default class CARequest {
-  activeQuarter: string;
-  activeQuarterID: number = 4420; // Bad but I needed to initialize it
-  quarterList: any;
+  activeQuarter: string; // The quarter used for any future searches
+  activeQuarterID: number = 4420; // The ID of the quarter, for part of the fetch request
+  quarterList: Array<any> = []; // Cache the list of {activeQuarter, activeQuarterID} pairs from courseavail
 
   constructor(quarter = "default") {
     this.activeQuarter = quarter;
@@ -33,14 +35,33 @@ export default class CARequest {
     });
   }
 
-  async getQuarters() {
-    let response = await fetch("/courseavail/autocomplete/quarters/");
-    let quarters = await response.json();
+  async getQuarters(): Promise<Array<{ value: string; label: string }>> {
+    // Check if the value is cached first
+    if (this.quarterList.length > 0) {
+      return this.quarterList;
+    }
 
-    return quarters.results.indb;
+    type CAQuarterList = {
+      title: string;
+      results: {
+        currdef: Object;
+        indb: Array<{ value: string; label: string }>;
+      };
+    };
+
+    // If it is not cached, retrieve from courseavail
+    try {
+      let response = await fetch("/courseavail/autocomplete/quarters/");
+      let quarters = (await response.json()) as CAQuarterList;
+      return quarters.results.indb;
+    } catch (error) {
+      console.error(error);
+      return [];
+    }
   }
 
   setActiveQuarter(quarter: string) {
+    // Compare param to value-label pairs in cached list of quarters
     this.quarterList.forEach((pair: any) => {
       if (pair.label == quarter) {
         this.activeQuarter = quarter;
@@ -50,21 +71,31 @@ export default class CARequest {
     this.activeQuarterID = this.activeQuarterID || this.quarterList[0].value;
   }
 
-  async getSearchResults(query: string) {
-    let response = await fetch(
-      `/courseavail/search/${this.activeQuarterID}/ugrad/${query}`
-    );
-    let queryMatches = await response.json();
-
-    return queryMatches;
+  async getSearchResults(query: string): Promise<Array<Object>> {
+    // Request query results from courseavail
+    try {
+      let response = await fetch(
+        `/courseavail/search/${this.activeQuarterID}/ugrad/${query}`
+      );
+      let queryMatches = await response.json();
+      return queryMatches.results;
+    } catch (error) {
+      console.error(error);
+      return [];
+    }
   }
 
-  async getCourseList() {
-    let response = await fetch(
-      `/courseavail/autocomplete/${this.activeQuarterID}/ugrad/courses`
-    );
-    let allCourses = response.json();
-
-    return allCourses;
+  async getCourseList(): Promise<Array<Object>> {
+    // Request all coursed being offered for the quarter from courseavail
+    try {
+      let response = await fetch(
+        `/courseavail/autocomplete/${this.activeQuarterID}/ugrad/courses`
+      );
+      let allCourses = await response.json();
+      return allCourses.results;
+    } catch (error) {
+      console.error(error);
+      return [];
+    }
   }
 }

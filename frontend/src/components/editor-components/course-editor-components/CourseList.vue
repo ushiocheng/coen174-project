e51c6d980fe0752018e0e09e0cc65b920896110c
Clover <32724585+ushiocheng@users.noreply.github.com>
2023-02-10 04:11:22 +0000

    WIP: CoursePicker and CourseList stuff; This may not build

--- frontend/src/components/editor-components/course-editor-components/CourseList.vue
+++ frontend/src/components/editor-components/course-editor-components/CourseList.vue
@@ -1,11 +1,38 @@
 <template>
-  
+  <v-container>
+    <v-row>
+      <v-col cols="12">
+        <course-picker />
+      </v-col>
+      <v-col cols="12">
+        <v-list>
+          <course-list-item v-for="(course, index) in courses" :key="index" :course="course"/>
+        </v-list>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import { defineComponent, computed } from 'vue';
+import CoursePicker from './CoursePicker.vue';
+import CourseListItem from './CourseListItem.vue';
+import { state } from '@/store/state';
 
-}
+export default defineComponent({
+  name: 'CourseList',
+  components: {
+    CoursePicker,
+    CourseListItem
+  },
+  setup() {
+    const courses = computed(() => state.courseSelected);
+
+    return {
+      courses
+    };
+  }
+});
 </script>
 
 <style>
--- frontend/src/components/editor-components/course-editor-components/CourseListItem.vue
+++ frontend/src/components/editor-components/course-editor-components/CourseListItem.vue
@@ -1,13 +1,44 @@
+// components/CourseListItem.vue
 <template>
-  
+  <v-list-item>
+    <v-list-item-content>
+      <v-list-item-title>{{ course.courseName }}</v-list-item-title>
+      <v-list-item-subtitle>{{ course.potentialSchedule }}</v-list-item-subtitle>
+    </v-list-item-content>
+    <v-list-item-action>
+      <v-btn @click="deleteCourse">Delete</v-btn>
+    </v-list-item-action>
+  </v-list-item>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import { defineComponent, inject } from 'vue';
+import { useCourseSelectedStore } from './state';
 
-}
+export default defineComponent({
+  props: {
+    course: {
+      type: Object as () => {
+        courseName: string;
+        potentialSchedule: number;
+      },
+      required: true,
+    },
+  },
+  setup(props, { emit }) {
+    const { deleteCourse } = useCourseSelectedStore();
+
+    const deleteCourse = () => {
+      const index = this.courseSelected.findIndex(
+        course => course.courseName === this.course.courseName
+      );
+      deleteCourse(index);
+    };
+
+    return { deleteCourse };
+  },
+});
 </script>
 
-<style>
-
+<style lang="scss">
 </style>

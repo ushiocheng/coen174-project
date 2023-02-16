19ab655fb89057c2d20370b445b69460b010f654
Anthony Bryson <antbryson17@gmail.com>
2023-02-16 11:28:46 -0800

    Polished the profileSwitcher class to handle multiple schedulers

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -20,51 +20,52 @@ registerPlugins(app);
 app.mount("#app");
 
 /*Testing*/
-import ScheduleSwitcher from "./classes/ScheduleSwitcher";
+import ProfileSwitcher from "./classes/ProfileSwitcher";
 
-let schedules = new ScheduleSwitcher();
+let profile = new ProfileSwitcher();
 
 (async () => {
   const start = Date.now();
 
   // Test creating schedules for different people
-  await schedules.createNewSchedule("Anthony's Schedule");
-  await schedules.createNewSchedule("Jackson's Schedule");
-  await schedules.createNewSchedule("Bad schedule");
-  console.log(schedules.getAllSchedules());
+  await profile.createNewProfile("Anthony's Schedule");
+  await profile.createNewProfile("Jackson's Schedule");
+  await profile.createNewProfile("Bad schedule");
+  console.log(profile.getAllProfiles());
 
   // Test deleting specific schedules
-  schedules.removeSchedule("Bad schedule");
-  console.log(schedules.getAllSchedules());
+  profile.removeProfile("Bad schedule");
+  console.log(profile.getAllProfiles());
+
+  // Test changing the profiles' names
+  profile.editProfileName("Anthony's Schedule", "Anthony");
+  profile.editProfileName("Jackson's Schedule", "Jackson");
+  console.log(profile.getAllProfiles());
 
   // Switching to and using one person's schedule
-  schedules.switchActiveSchedule("Jackson's Schedule");
-  await schedules.changeQuarter("Winter 2022");
-  await schedules.includeCourse("ACTG 131"); // Returns true if successfully added, false otherwise
-  await schedules.includeCourse("ACTG 136");
-  await schedules.includeCourse("AMTH 106");
-  await schedules.includeCourse("AMTH 108");
-  console.log(`${schedules.getActiveScheduleName()}'s schedules:`);
-  console.log(schedules.getSchedules());
+  profile.switchActiveProfile("Jackson");
+  await profile.changeQuarter("Winter 2022");
+  await profile.includeCourse("ACTG 131"); // Returns true if successfully added, false otherwise
+  await profile.includeCourse("ACTG 136");
+  await profile.includeCourse("AMTH 106");
+  await profile.includeCourse("AMTH 108");
+  console.log(`${profile.getActiveProfileName()}'s schedules:`);
+  console.log(profile.getSchedules());
 
   // Switching to and using another person's schedule
-  schedules.switchActiveSchedule("Anthony's Schedule");
-  await schedules.changeQuarter("Fall 2022");
-  await schedules.includeCourse("COEN 140");
-  await schedules.includeCourse("COEN 146");
-  await schedules.includeCourse("COEN 145");
-  await schedules.includeCourse("COEN 179");
-  console.log(`${schedules.getActiveScheduleName()}'s schedules:`);
-  console.log(schedules.getSchedules());
+  profile.switchActiveProfile("Anthony");
+  await profile.changeQuarter("Fall 2022");
+  await profile.includeCourse("COEN 140");
+  await profile.includeCourse("COEN 145");
+  await profile.includeCourse("COEN 146");
+  await profile.includeCourse("COEN 179");
+  console.log(`${profile.getActiveProfileName()}'s schedules:`);
+  console.log(profile.getSchedules());
 
-  schedules.switchActiveSchedule("Jackson's Schedule");
-  console.log(`${schedules.getActiveScheduleName()}'s schedules:`);
-  console.log(schedules.getSchedules());
-
-  // console.log("Generating schedules for:");
-  // console.log(scheduler.selectedCourses);
-  // scheduler.buildSchedules();
-  // console.log(scheduler.scheduleList);
+  // Switch back to a saved profile
+  profile.switchActiveProfile("Jackson");
+  console.log(`${profile.getActiveProfileName()}'s schedules:`);
+  console.log(profile.getSchedules());
 
   const end = Date.now();
   console.log("runtime: " + (end - start) / 1000);
